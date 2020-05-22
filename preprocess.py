e48ba1192d2ec748990b1ee272923aaa4be6c3ff
gaurav <gauravsinha952@gmail.com>
2020-05-22 23:16:39 +0530

    Implemented main function: preprocess

--- preprocess.py
+++ preprocess.py
@@ -164,3 +164,85 @@ def tokenize(sent):
     ['Bob', 'dropped', 'the', 'apple', '.', 'Where', 'is', 'the', 'apple', '?']
     '''
     return [x.strip() for x in re.split('(\W+)?', sent) if x.strip()]
+
+
+
+def process_data(KB_file, data_file):
+    entities, relations = read_KB(KB_file)
+    words, data, sentence_size = read_data(data_file)
+
+    word2id = {}
+    ent2id = {}
+    rel2id = {}
+
+    word2id['<unk>'] = 0
+    rel2id['<end>'] = 0
+    ent2id['<unk>'] = 0
+
+    for r in relations:
+        # same r_id in rel2id and word2id
+        if r not in rel2id.keys():
+            rel2id[r] = len(rel2id)
+        if r not in word2id.keys():
+            word2id[r] = len(word2id)
+    for e in entities:
+        if e not in ent2id.keys():
+            ent2id[e] = len(ent2id)
+    for word in words:
+        if word not in word2id.keys():
+            word2id[word] = len(word2id)
+
+    print('here are %d words in word2id(vocab)' % len(word2id))
+    print('here are %d relations in rel2id(rel_vocab)' % len(rel2id))
+    print('here are %d entities in ent2id(ent_vocab)' % len(ent2id))
+
+    Triples, KBs, tails_size = get_KB(KB_file, ent2id, rel2id)
+
+    print("The number of records or triples", len(np.nonzero(KBs)[0]))
+
+    Q = []
+    QQ = []
+    A = []
+    AA = []
+    P = []
+    PP = []
+    S = []
+    SS = []
+
+    for query, answer, path, answerset in data:
+        path = path.strip().split('#')  # path = [s,r1,m,r2,t]
+        #answer = path[-1]
+
+        query = query.strip().split()
+        ls = max(0, sentence_size-len(query))
+        q = [word2id[w] for w in query] + [0] * ls
+        Q.append(q)
+        QQ.append(query)
+
+
+        a = np.zeros(len(ent2id))  # if use new ans-vocab, add 0 for 'end'
+        a[ent2id[answer]] = 1
+        A.append(a)
+        AA.append(ent2id[answer])
+
+        p = []
+        for i in range(len(path)):
+            if i % 2 == 0:
+                e = ent2id[path[i]]
+                p.append(e)
+            else:
+                r = rel2id[path[i]]
+                p.append(r)
+
+        P.append(p)
+        PP.append(path)
+
+        anset = answerset.split('/')
+        anset = anset[:-1]
+        ass = []
+        for a in anset:
+            ass.append(ent2id[a])
+        S.append(ass)
+        SS.append(anset)
+
+    return np.array(Q), np.array(A), np.array(P), np.array(S), Triples, sentence_size, word2id, ent2id, rel2id
